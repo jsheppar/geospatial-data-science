c302a4a2ceda2bb862a8f6ed3a4952fa4b0de57b
jsheppar <jsheppar@uoregon.edu>
2022-01-04 11:49:51 -0800

    uploaded first assignment

---
+++ labs/lab1/submitting_assignments.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan  4 11:28:21 2022
+
+@author: jsheppard
+"""
+
+import numpy as np
+a = 3
+b = 2
+c = np.multiply(a, b)
+print(c)
